b92045c94bb8b6fde5d119d6fcab73adc9e2bcb7
Jennifer Klay <jklay@calpoly.edu>
2014-04-10 14:05:47 -0700

    creating section 03 repo

---
+++ ConfigureGit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+git config --global user.name "Jennifer Klay"
+git config --global user.email "jklay@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
